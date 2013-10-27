02498ee838d30891303041ddedd6905abee6fb69
Angus Scott <angusscott@angus-scotts-macbook-pro.local>
2013-10-27 09:51:22 +0000

    Cleaned up facetracekr

--- facetrack.py
+++ facetrack.py
@@ -9,11 +9,11 @@ def isFace():
 		image = cam.getImage().flipHorizontal().scale(0.25)
 		faces = image.findHaarFeatures( os.getcwd() + "/haarcascade_frontalface_alt.xml")
 		
-		if faces:
-		 	face = faces[-1]
-		 	face.draw(Color.RED, 1)
-		 	image.show()
-		else:
-			return "Break connection"
+		if faces :
+		 	for face in faces:
+		 		face.draw(Color.RED, 1)
+		image.show()
+		# else:
+		# 	return "Break connection"
 
 isFace()
