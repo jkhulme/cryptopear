d5b7ea3212bcd2b1eadefee2e6dd10ca74ee21f7
Aaron Cronin <cronin@include.cat>
2013-10-27 01:29:50 +0000

    Names+++

--- chat_server/lib/namespoofer.rb
+++ chat_server/lib/namespoofer.rb
@@ -7,7 +7,7 @@ class NameSpoofer
   def random_first_name
     pool= %w{walter jesse james jack wolf terrence
     tom billy bartholomew hugh stephen lynne lisa alice
-    noodles snoop jerome tyrone p-dawg bjorn jesper
+    noodles snoop jerome tyrone p-Dawg bjorn jesper
     william bob mary ryan paul daniel clarence margaret}
     pool.sample.capitalize
   end
